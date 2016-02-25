8ebe9566290d1f48797744ed97d0734d3ea64425
vertesy <a.vertesy@hubrecht.eu>
2016-02-25 15:17:32 +0100

    Resolves #2 and fixes #7 - Debug, and overview.md

--- RoxygenReady/R/RoxygenReady.R
+++ RoxygenReady/R/RoxygenReady.R
@@ -45,16 +45,17 @@ toClipboard <- function(x, sep="\t", header=FALSE, row.names=FALSE, col.names =F
 #' Read in a file, annotate and write out all functions documentation with Roxygen skeleton (FileToFile)
 #' @param FileWithFunctions An .R file containing all the function definitions of your library
 #' @param outFile Filename and path where the annotated version will be written out
+#' @param overview If true, a function overview .md file is created next to your input file. See rr_function_overview_document() for details.
 #' @examples
 #' @export
 
-RoxygenReady <-function (FileWithFunctions, outFile = kollapse(FileWithFunctions, ".annot.R", print = F)) {
+RoxygenReady <-function (FileWithFunctions, outFile = kollapse(FileWithFunctions, ".annot.R", print = F, overview = T)) {
 	x = strsplit(FileWithFunctions, split = "/")[[1]]
 	ScriptName = x[length(x)]
 	write(kollapse("## ", ScriptName, "\n\n", print = F), file = outFile)
 	list_of_functions = rr_extract_all_function_names_in_a_script(FileWithFunctions)
 	funnames = names(list_of_functions)
-	commentz = rr_extract__all_descriptions_from_comment(FileWithFunctions)
+	commentz = rr_extract_all_descriptions_from_comment(FileWithFunctions)
 	for (i in 1:length(list_of_functions)) {
 		function_to_parse = list_of_functions[[i]]
 		if (!is.function(function_to_parse)) {
@@ -78,6 +79,7 @@ RoxygenReady <-function (FileWithFunctions, outFile = kollapse(FileWithFunctions
 		code_ = gsub(paste0("  ", "  "), "\t", code_, perl = T)
 		code = c(kollapse(funnames[i], " <-", code_[1:2], print = F), code_[3:length(code_)], sep = "\n")
 		write(code, file = outFile, append = T)
+		if (overview) { rr_function_overview_document(FileWithFunctions) }
 	}
 }
 
@@ -101,14 +103,14 @@ rr_extract_all_function_names_in_a_script <-function (inFile) {
 }
 
 
-#' rr_extract__all_descriptions_from_comment
+#' rr_extract_all_descriptions_from_comment
 #'
 #' Scan a script for (descriptive) comments in the first line of each function's definition.
 #' @param inFile input file with function definitions to be scanned
-#' @examples rr_extract__all_descriptions_from_comment (inFile =  )
+#' @examples rr_extract_all_descriptions_from_comment (inFile =  )
 #' @export
 
-rr_extract__all_descriptions_from_comment <-function (inFile) {
+rr_extract_all_descriptions_from_comment <-function (inFile) {
 	ScriptAsStringsPerLine = readLines(inFile)
 	patt = " *<- *function *\\(.+"
 	index = grep(patt, ScriptAsStringsPerLine, perl = T)
@@ -133,40 +135,66 @@ rr_extract_default_args <-function (function_to_parse) {
 	return(paste(names(a), a, sep = " = ", collapse = ", "))
 }
 
-#' RoxygenReady.MemoryToClipboard
+#' rr_function_overview_document
+#'
+#' Create a Markdown document with a numbered list of all functions and their descriptions from the "inFile", saved right next to it!
+#' @param inFile File with the funcitons to be listed and displayed in a .md file.
+#' @param outFile The location of the created markdown document with the list of functions in the  "inFile". By default, it saves next to it, with the extension ".FunctionOverview.md".
+#' @examples rr_function_overview_document (inFile =  , outFile = paste0(inFile, ".FunctionOverview.md"))
+#' @export
+
+rr_function_overview_document <-function (inFile, outFile = paste0(inFile, ".FunctionOverview.md") ) { # Create a Markdown document with a numbered list of all functions and their descriptions from the "inFile", saved right next to it!
+	FunctionNames = names(rr_extract_all_function_names_in_a_script(inFile) )
+	n = length(FunctionNames)
+	FunctionNames = paste0("### ", (1:n), ". ",names(rr_extract_all_function_names_in_a_script(inFile) ) )
+	Descriptions = paste0("- ", rr_extract_all_descriptions_from_comment(inFile) )
+
+	FunctionNames = FunctionNames[order(FunctionNames)]
+	Descriptions = Descriptions[order(FunctionNames)] # Reorder by
+	body = paste0( "\n\n", FunctionNames, "\n", Descriptions)
+	write("## Function Overview", file = outFile)
+	write("You find the list of function of this library below. For details, please use the `help()` function. <br>", file = outFile, append = T)
+	write(body, file = outFile, append = T)
+
+}
+
+
+#' RoxygenReady.MemoryToClipboard.singleFunction
 #'
 #' Compile a single functions documentation for Roxygen
 #' @param function_to_parse Name of the function with the arguments of interest.
 #' @examples RoxygenReady.MemoryToClipboard (function_to_parse =  )
 #' @export
 
-RoxygenReady.MemoryToClipboard <-function (function_to_parse) {
+RoxygenReady.MemoryToClipboard.singleFunction <-function (function_to_parse) { #' Compile a single functions documentation for Roxygen
 	if (!is.function(function_to_parse)) {
 		any_print("No function called", function_to_parse)
 	}
 	s = "#' "
 	desc = NULL
-	funname = substrRight(match.call()[1], n = 100)
+	funname = substitute(function_to_parse)
+	commentz =gsub(".+# ", "", capture.output(function_to_parse) [1])
 	desc[[1]] = kollapse(s, funname, " ", print = F)
 	desc[[2]] = s
-	desc[[3]] = kollapse(s, "Copy fun. description from inline comments", print = F)
+	desc[[3]] = kollapse(s, commentz, print = F)
 	argz = names(formals(fun = function_to_parse))
 	i = 2
 	for (a in argz) {
 		i = i + 1
 		desc[[i + 1]] = kollapse(s, "@param ", a, " ", print = F)
 	}
-	desc[[:length(argz) + 4]] = kollapse(s, "@examples ", match.call(), print = F)
-	desc[[:length(argz) + 5]] = kollapse(s, "@export")
-	toClipboard(desc)
+	desc[[length(argz) + 4]] = kollapse(s, "@examples ", funname, " (", rr_extract_default_args(function_to_parse), ")", print = F)
+	desc[[length(argz) + 5]] = kollapse(s, "@export \n", print = F)
 	print(desc, quote = F)
-	print("", quote = F)
-	print("", quote = F)
+	code_ = noquote(deparse(function_to_parse, width.cutoff = 100L))
+	code_ = print(gsub("	", "\t", code_, perl = T))
+	code = c(kollapse(funname, " <-", code_[1:2], print = F), code_[3:length(code_)], sep = "\n")
+	toClipboard(c(desc, code))
 	print("		 >>> Function header is also copied to your clipboard")
 }
 
 
-#' RoxygenReady.singleFunction.MemoryToFile
+#' RoxygenReady.MemoryToFile.singleFunction
 #'
 #' Compile and write out a single functions documentation for Roxygen
 #' @param function_to_parse Name of the function with the arguments of interest.
@@ -174,67 +202,33 @@ RoxygenReady.MemoryToClipboard <-function (function_to_parse) {
 #' @examples RoxygenReady.singleFunction.MemoryToFile (function_to_parse =  , outFile = )
 #' @export
 
-RoxygenReady.singleFunction.MemoryToFile <-function (function_to_parse, outFile) {
+
+RoxygenReady.MemoryToFile.singleFunction <-function (function_to_parse, outFile) {
 	if (!is.function(function_to_parse)) {
 		any_print("No function called", function_to_parse)
 	}
 	s = "#' "
 	desc = NULL
-	funname = substrRight(match.call()[1], n = 100)
+	funname = substitute(function_to_parse)
+	commentz =gsub(".+# ", "", capture.output(function_to_parse) [1])
 	desc[[1]] = kollapse(s, funname, " ", print = F)
 	desc[[2]] = s
-	desc[[3]] = kollapse(s, "Copy fun. description from inline comments", print = F)
+	desc[[3]] = kollapse(s, commentz, print = F)
 	argz = names(formals(fun = function_to_parse))
 	i = 2
 	for (a in argz) {
 		i = i + 1
 		desc[[i + 1]] = kollapse(s, "@param ", a, " ", print = F)
 	}
-	desc[[:length(argz) + 4]] = kollapse(s, "@examples ", match.call(), print = F)
-	desc[[:length(argz) + 5]] = kollapse(s, "@export")
-	toClipboard(desc)
+	desc[[length(argz) + 4]] = kollapse(s, "@examples ", funname, " (", rr_extract_default_args(function_to_parse), ")", print = F)
+	desc[[length(argz) + 5]] = kollapse(s, "@export \n", print = F)
 	print(desc, quote = F)
 	write(desc, file = outFile)
 	write("", file = outFile, append = T)
 	code_ = noquote(deparse(function_to_parse, width.cutoff = 100L))
 	code_ = print(gsub("	", "\t", code_, perl = T))
 	code = c(kollapse(funname, " <-", code_[1:2], print = F), code_[3:length(code_)], sep = "\n")
+	# toClipboard(c(desc, code))
 	write(code, file = outFile, append = T)
 }
 
-
-#' RoxygenReady.old
-#'
-#' Compile and write out all functions documentation in a script for Roxygen
-#' @param FuncNames a vector of function names (of functions in the memory space) to be annotated
-#' @param outFile Filename and path where the annotated version will be written out
-#' @examples descriptor_roxy_old (FuncNames =  , outFile)
-#' @export
-
-RoxygenReady.old <-function (FuncNames, outFile) {
-	write(kollapse("## ", substitute(FuncNames), " \n\n", print = F), file = outFile)
-	funnames = names(FuncNames)
-	for (i in 1:length(FuncNames)) {
-		function_to_parse = get(funnames[i])
-		if (!is.function(function_to_parse)) {
-			any_print("No function called", funnames[i])
-		}
-		s = "#' "
-		desc = NULL
-		desc[[1]] = kollapse(s, funnames[i], " ", print = F)
-		desc[[2]] = s
-		desc[[3]] = kollapse(s, "Description ...", print = F)
-		argz = names(formals(fun = function_to_parse))
-		nr_of_args =length(argz)
-		for (j in 1:nr_of_args) {
-			desc[[j + 3]] = kollapse(s, "@param ", argz[j], " ", print = F)
-		}
-		desc[[nr_of_args + 4]] = kollapse(s, "@examples ", match.call(), print = F)
-		desc[[nr_of_args + 5]] = kollapse(s, "@export \n")
-		write(desc, file = outFile, append = T)
-		code_ = noquote(deparse(function_to_parse, width.cutoff = 100L))
-		code_ = print(gsub("	", "\t", code_, perl = T))
-		code = c(kollapse(funnames[i], " <-", code_[1:2], print = F), code_[3length(code_)], sep = "\n")
-		write(code, file = outFile, append = T)
-	}
-}
